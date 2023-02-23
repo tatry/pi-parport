782cfec26e92fd5dac95a2ef01d709cbf0e1ff06
Jan Palimąka <25018671+tatry@users.noreply.github.com>
2023-02-23 23:34:03 +0100

    Improve DKMS support

--- install.sh
+++ install.sh
@@ -13,6 +13,17 @@ THIS_REPO=`pwd`
 cp -R "$THIS_REPO/driver" /usr/src/pi-parport-1.0
 dkms install -m pi-parport -v 1.0
 
+# For some reason modules are not rebuild for new kernel, so force build
+cat <<EOF >> /etc/kernel/postinst.d/pi-parport
+#!/bin/sh
+
+version="\$1"
+dkms install -m pi-parport -v 1.0 -k "\${version}"
+
+exit 0
+EOF
+chmod +x /etc/kernel/postinst.d/pi-parport
+
 # Install device tree overlay
 cd "$THIS_REPO/dts"
 make
